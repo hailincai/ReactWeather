0bc802800beddbabcae33682fca1d7fb4cbc09aa
hailin.cai <hailin.cai.neulion@gmail.com>
2017-01-04 17:02:23 -0500

    prep repo for heroku

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require("express");
 
 var app = express();
 
+//get binding port from heroku env
+const PORT = process.env.PORT || 8080;
+
+app.use(function(req, res, next) {
+  if (req.headers["x-forwarded-proto"] === "http") {
+    next();
+  } else {
+    res.redirect("http://" + req.hostname + req.url);
+  }
+});
 app.use(express.static("public"));
 
-app.listen(8080, function(){
+app.listen(PORT, function(){
   console.log("Server start up at 8080");
 });
